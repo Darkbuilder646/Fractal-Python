2f6ecc7924b698a54cc9001a1327fd65ddd6295b
Darkbuiler646 <theolejeune.tl@gmail.com>
2023-10-08 16:24:29 +0200

    Generate Fractal with image + add color

--- Mandelbrot/MandelbrotFractal.py
+++ Mandelbrot/MandelbrotFractal.py
@@ -1,47 +1,43 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
+
+def CustomCmap():
+    colors = [(0, 0, 1), (0, 0, 0)]
+    cmap = LinearSegmentedColormap.from_list('custom_cmap', colors, N=100)
+    return cmap
 
 
-def Computes_Z(complex, max_itteration = 100):
+def ComputesZ(complex, max_iterations = 100):
     z = 0
-    for i in range(max_itteration):
+    for i in range(max_iterations):
         z = z * z + complex
         yield z
 
-def ComputesMandelbrot(complex, max_itteration = 100):
-    for n, z_nbr in enumerate(Computes_Z(complex, max_itteration)):
+def ComputesMandelbrot(complex, max_iterations = 100):
+    for n, z_nbr in enumerate(ComputesZ(complex, max_iterations)):
         if(abs(z_nbr) > 2.0):
             return n # => the sequence diverges
         
-    return max_itteration # => the sequence does not diverge
-    
-fig, ax = plt.subplots()
+    return max_iterations # => the sequence does not diverge
+  
 
-def DrawMandelbrot(start: float, end: float, step: float, max_itteration = 100):
-    for x in np.arange(start, end+step, step):
-        for y in np.arange(start, end+step, step):
-            result = ComputesMandelbrot(x + y * 1j, max_itteration)
-            if(result != max_itteration):
-                point = plt.Circle((x, y), 0.015, color="white")
-                ax.add_artist(point)
-            else:
-                point = plt.Circle((x, y), 0.015, color="black")
-                ax.add_artist(point)
+def DrawMandelbrot(startX: float, endX: float, startY: float, endY: float, width, height, max_iterations=100):
+    img = np.zeros((height, width))
 
-DrawMandelbrot(-2.5, 1.5, 0.05, 100)
+    for x in range(width):
+        for y in range(height):
+            realNbr = startX + (x / (width - 1)) * (endX - startX)
+            imagNbr = startY + (y / (height - 1)) * (endY - startY)
+            c = complex(realNbr, imagNbr)
+            color = ComputesMandelbrot(c, max_iterations)
+            img[y, x] = color
 
-fig.canvas.draw()
-width, height = fig.canvas.get_width_height()
-image_array = np.array(fig.canvas.renderer.buffer_rgba())
+    #cmap = CustomCmap()
+    #default cmap = 'PuBu_r'
+    plt.imshow(img, extent=(startX, endX, startY, endY), cmap='PuBu_r', origin='lower', vmax=max_iterations, vmin=0, interpolation='bilinear')
+    plt.colorbar()
+    plt.show()
 
-ax.set_xlim(-2.5,1)
-ax.set_ylim(-1.5,1.5)
 
-ax.axhline(0, color="black", linewidth=0.5)
-ax.axvline(0, color="black", linewidth=0.5)
-
-plt.xlabel("Real")
-plt.ylabel("imaginary")
-
-plt.imshow(image_array)
-plt.show()
+DrawMandelbrot(-2.0, 1.0, -1.5, 1.5, 1000, 1000, 100)
